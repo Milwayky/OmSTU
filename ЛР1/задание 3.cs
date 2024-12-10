a2cae055fd12f61a803d75835a7c0d4af136fb17
Milwayky <vyryshevaalisa@gmail.com>
2024-12-10 11:19:17 +0600

    Update задание 3.cs

--- ЛР1/задание 3.cs
+++ ЛР1/задание 3.cs
@@ -1,9 +1,8 @@
 using System;
-
- public class Program1
- {
-  public static void Main()
-  {
+public class Program1
+{
+    public static void Main()
+    {
         int l = Convert.ToInt32(Console.ReadLine());
         int m = Convert.ToInt32(Console.ReadLine());
         int p = Convert.ToInt32(Console.ReadLine());
@@ -11,5 +10,5 @@ using System;
         int perimetr = 2*(l + m);
         int rasstoyanie = perimetr*n + 2*p*n + l*(n-1)*n;
         Console.WriteLine(rasstoyanie);
-  }
- }
+    }
+}
