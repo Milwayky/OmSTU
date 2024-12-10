1a28c7e13e5554058d2c27a9ccd95bcc219dfeb4
Milwayky <vyryshevaalisa@gmail.com>
2024-12-10 11:32:15 +0600

    Create задание 2.cs

---
+++ ЛР3/задание 2.cs
@@ -0,0 +1,33 @@
+using System;
+class HelloWorld
+{
+  static void Main()
+  {
+    int n = Convert.ToInt32(Console.ReadLine());
+    int MmaxCounter = 0;
+    int Counter = 0;
+    int Last = -1;
+    for (int i = 0; i < n; i++)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            if (a == Last)
+            {
+                Counter++;
+            }
+            else
+            {
+                Last = a;
+                Counter = 1;
+            }
+            MmaxCounter = Math.Max(MmaxCounter, Counter);
+        }
+        else
+        {
+            Counter = 0;
+        }
+    }
+    Console.WriteLine(MmaxCounter);
+  }
+}
