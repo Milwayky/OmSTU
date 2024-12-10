f48c3d581be750645d652bfa7d61571c4d94cd7b
Milwayky <vyryshevaalisa@gmail.com>
2024-12-10 11:26:32 +0600

    Create задание 3.cs

---
+++ ЛР2/задание 3.cs
@@ -0,0 +1,37 @@
+3 задание
+
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int counterNechet = 0;
+        int n = Convert.ToInt32(Console.ReadLine());
+        int mmax1 = 0;
+        int mmax2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (i == 0)
+            {
+                mmax2 = a;
+                mmax1 = a;
+            }
+            if (a > mmax1)
+            {
+                mmax1 = a;
+            }
+            else if(a > mmax2)
+            {
+                mmax2 = a;
+            }
+            else
+            {
+                continue;
+            }
+     
+        }
+        Console.WriteLine(mmax2);
+    }
+}
