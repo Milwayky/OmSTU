e8924e2a6fc19c17d3baec00da1c7f75fe9f77b2
Milwayky <vyryshevaalisa@gmail.com>
2024-12-10 11:31:55 +0600

    Create задание 1.cs

---
+++ ЛР3/задание 1.cs
@@ -0,0 +1,32 @@
+using System;
+class HelloWorld 
+{
+    static void Main() 
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int Mmin = n + 1;
+        int Counter1 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
+            {
+                Counter1 += 1;
+            }
+            else
+            {
+                if (Counter1 > 0)
+                {
+                    Mmin = Math.Min(Mmin, Counter1);
+                    Counter1 = 0;
+                }
+            }
+        }
+        if (Counter1 > 0)
+        {
+            Mmin = Math.Min(Mmin, Counter1);
+            Counter1 = 0;
+        }
+        Console.WriteLine(Mmin);
+    }
+}
