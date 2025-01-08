151668e5e73eab21d9d0c1f9baf138d751999233
Milwayky <vyryshevaalisa@gmail.com>
2025-01-08 17:27:25 +0500

    Update задание 1.cs

--- ЛР8/задание 1.cs
+++ ЛР8/задание 1.cs
@@ -1,16 +1,12 @@
 using System;
-
 public class HelloWorld
 {
     static void Main()
     {
         string stroka = Console.ReadLine();
         string[] words = stroka.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-        foreach (string slovo in words)
-        {
-            Console.Write(slovo);
-            Console.Write(' ');
-        }
+        string result = string.Join(" ", words);
+        Console.WriteLine(result);
 
     }
 }
