0ed97e38762d3c14eb59e27b28b5721808ec0ced
Milwayky <vyryshevaalisa@gmail.com>
2025-01-08 17:17:19 +0500

    Update задание 1.cs

--- ЛР6/задание 1.cs
+++ ЛР6/задание 1.cs
@@ -1,67 +1,76 @@
+//отсортировать строки по убыванию количества нулевых элементов
+
 using System;
 class HelloWorld 
 {
-  static void Main() 
-  {
-    int m = Convert.ToInt32(Console.ReadLine());
-    int n = Convert.ToInt32(Console.ReadLine());
-    int[,] massiv = new int[m, n];
-    
-    
-    for (int i = 0; i < m; i++) 
+    static void Main() 
     {
-      for (int j = 0; j < n; j++)
-      {
-        massiv[i, j] = Convert.ToInt32(Console.ReadLine());
-      }
-    }
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] massiv = new int[m, n];
     
-    
-    for (int column1 = 0; column1 < n - 1; column1++) 
+        int mmax = 0, mmin = n + 1;
+        for (int i = 0; i < m; i++) 
         {
-            for (int column2 = column1 + 1; column2 < n; column2++) 
+            for (int j = 0; j < n; j++)
             {
-                int sum1 = 0, sum2 = 0;
-                int prod1 = 1, prod2 = 1;
-                int zero_counter_1 = 0, zero_counter_2 = 0;
-
-                for (int row = 0; row < m; row++) 
+                massiv[i, j] = Convert.ToInt32(Console.ReadLine());
+                
+                
+                if (massiv[i, j] > mmax)
                 {
-                    int perevemuty_massiv_1 = massiv[row, column1];
-                    int perevemuty_massiv_2 = massiv[row, column2];
-
-                    sum1 += perevemuty_massiv_1;
-                    sum2 += perevemuty_massiv_2;
-
-
-                    if (perevemuty_massiv_1 != 0)
-                    {
-                        prod1 *= perevemuty_massiv_1;
-                    }
-                    if (perevemuty_massiv_2 != 0)
-                    {
-                        prod2 *= perevemuty_massiv_2;
-                    }
-
-
-                    if (perevemuty_massiv_1 == 0) 
-                    {
-                        zero_counter_1 += 1;
-                    }
-                    if (perevemuty_massiv_2 == 0)
-                    {
-                        zero_counter_2 += 1;
-                    }
-                    
-
+                    mmax = massiv[i, j];
                 }
-
-
-                if ((sum1 == sum2) && (prod1 == prod2) && (zero_counter_1 == zero_counter_2)) 
+                if (massiv[i, j] < mmin)
                 {
-                    Console.WriteLine($"Совпадают столбцы: {column1 + 1} и {column2 + 1}");
+                    mmin = massiv[i, j];
                 }
-            }   
+            }
+        }
+        
+        
+        int[] massiv_zero = new int[m];
+        for (int i = 0; i < m; i++)
+        {
+            int counter = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (massiv[i, j] == 0)
+                {
+                    counter += 1;
+                }
+            }
+            massiv_zero[i] = counter;
+        }
+        
+        for (int i = 0; i < m - 1; i++)
+        {
+            for (int j = i + 1; j < m; j++)
+            {
+                if (massiv_zero[i] < massiv_zero[j])
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        int vrenemennyi_massiv = massiv[i, k];
+                        massiv[i, k] = massiv[j, k];
+                        massiv[j, k] = vrenemennyi_massiv;
+                    }
+                    int vrenemennyi_massiv_zero = massiv_zero[i];
+                    massiv_zero[i] = massiv_zero[j];
+                    massiv_zero[j] = vrenemennyi_massiv_zero;
+                }
+            }
+        }
+
+        
+        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"{massiv[i, j]} \t");
+            }
+                Console.WriteLine();
         }
     }
 }
