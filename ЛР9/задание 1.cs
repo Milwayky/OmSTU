38854eed20392ddc6e78575ac9dcad308f8b3d55
Milwayky <vyryshevaalisa@gmail.com>
2025-01-10 00:52:00 +0500

    Update задание 1.cs

--- ЛР9/задание 1.cs
+++ ЛР9/задание 1.cs
@@ -5,6 +5,7 @@ public class HelloWorld
     static void Main()
     {
         string stroka = Console.ReadLine();
+        int maxlen = 0;
         int len = 0;
         int letter = 0;  // 0 - x, 1 - y, 2 - z
         foreach (char i in stroka)
@@ -16,6 +17,11 @@ public class HelloWorld
                     letter = 1;
                     len += 1;
                 }
+                else
+                {
+                    maxlen = Math.Max(maxlen, len);
+                    len = 0;
+                }
             }
             
             else if (letter == 1)
@@ -25,7 +31,20 @@ public class HelloWorld
                     letter = 2;
                     len += 1;
                 }
+                else if (i == 'X')
+                {
+                    maxlen = Math.Max(maxlen, len);
+                    len = 1;
+                    letter = 1;
+                }
+                else
+                {
+                    maxlen = Math.Max(maxlen, len);
+                    len = 0;
+                    letter = 0;
+                }
             }
+            
             else if (letter == 2)
             {
                 if (i == 'Z')
@@ -33,8 +52,21 @@ public class HelloWorld
                     letter = 0;
                     len += 1;
                 }
+                else if (i == 'X')
+                {
+                    maxlen = Math.Max(maxlen, len);
+                    len = 1;
+                    letter = 1;
+                }
+                else
+                {
+                    maxlen = Math.Max(maxlen, len);
+                    len = 0;
+                    letter = 0;
+                }
             }
         }
-        Console.WriteLine(len);
+        maxlen = Math.Max(maxlen, len);
+        Console.WriteLine(maxlen);
     }
 }
